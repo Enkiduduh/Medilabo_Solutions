9732ed7ff2cf637b45a979e71b8042d2751ad4b5
Ilyes Soumar Djouma <ilyes.soumar@free.fr>
2025-08-13 23:28:52 +0200

    added app.yul and gateway param

--- backend/gateway/src/main/java/com/medilabo/gateway/GatewayApplication.java
+++ backend/gateway/src/main/java/com/medilabo/gateway/GatewayApplication.java
@@ -1,7 +1,11 @@
 package com.medilabo.gateway;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class GatewayApplication {
@@ -10,4 +14,15 @@ public class GatewayApplication {
 		SpringApplication.run(GatewayApplication.class, args);
 	}
 
+	// Route hardcodée pour tester : /api/** -> http://localhost:8080
+	@Bean
+	RouteLocator routes(RouteLocatorBuilder b) {
+		return b.routes()
+				.route("patient-service", r -> r
+						.path("/api/**")
+						.uri("http://localhost:8080"))
+				.build();
+	}
+	@PostConstruct
+	void started() { System.out.println(">> Gateway started on 8081 with /api/** route"); }
 }
